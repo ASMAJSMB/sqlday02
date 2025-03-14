1082575e72b7a7b644b4026b2e4f0763a097f5b5
ASMAJSMB <haddadasma006@gmail.com>
2025-03-14 14:25:30 +0100

    Add files via upload

---
+++ exo03.sql
@@ -0,0 +1,12 @@
+INSERT into etudiants (id_etudiant, nom, prenom, email)
+VALUES
+('1','haddad','asma','haddadasma006@gmail.com'),
+('2','haddad','mamine','mamineh@gmail.com'),
+('3','haddad','anais','anais@gmail.com');
+
+-- deuxieme question 
+insert into cours (id_cours,nom_cours)
+VALUES
+('1','dev'),
+('2','design');
+-- 
---
+++ exo04.sql
@@ -0,0 +1,4 @@
+select * from etudiants;
+select nom ,prenom from etudiants ;
+select * from cours 
+where nom_cours like '%a%';
